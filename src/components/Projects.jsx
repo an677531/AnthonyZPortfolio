2440a1adc3aafe03ce829ffa2dbd952b5f1790f6
an677531 <#Az5118200>
2026-09-02 17:09:42 -0400

    projects page button background buttons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,43 +3,36 @@ function Projects({ onNavigate }) {
         <section id="projects" className="hero">
             <div className="container">
 
-                {/**<button
-                    className="project-button"
-                    onClick={() => onNavigate("project-template")}
-                >
-                    Main Project
-                </button>*/}
-
                 <button
-                    className="project-button"
+                    className="project-button preserv"
                     onClick={() => onNavigate("project-preserv")}
                 >
                     Preserv
                 </button>
 
                 <button
-                    className="project-button"
+                    className="project-button recipe-jungle"
                     onClick={() => onNavigate("project-recipe-jungle")}
                 >
                     Recipe Jungle
                 </button>
 
                 <button
-                    className="project-button"
+                    className="project-button minigov"
                     onClick={() => onNavigate("project-minigov")}
                 >
                     MiniGov
                 </button>
 
                 <button
-                    className="project-button"
+                    className="project-button ucf"
                     onClick={() => onNavigate("project-UCF")}
                 >
                     University of Central Florida
                 </button>
 
                 <button
-                    className="project-button"
+                    className="project-button graphic-design"
                     onClick={() => onNavigate("project-graphic-design")}
                 >
                     Graphic Design Projects
